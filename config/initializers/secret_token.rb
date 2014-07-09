0702df944b8e45070a854f8fb8b8c9c57f3d9dfb
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-07-09 16:23:00 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ObjectivesTracker::Application.config.secret_key_base = '9e633646f5679bc076143508b2a4e95754729edebcad3a843143e0e5f8e0638ef58cb0c106b96d7684ad16fa5bec0cf7723eca5bbcf3438ce8093fc35f25396c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ObjectivesTracker::Application.config.session_store :cookie_store, key: '_objectivesTracker_session'
