6fae63d7eff48540614de7e89f896ded97757bb4
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-07-31 18:03:20 -0300

    Improvements on authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 
   def self.auth(email, password)
-    User.where(email: email, password: password)
+    User.where(email: email, password: password).first
+  end
+
+  def public_data
+    { name: self.name, email: self.email }
   end
 end
