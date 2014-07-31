fca29c4554140eff5a347539a03027afbb2858e1
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2014-07-31 19:03:34 -0300

    Remove conflict

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,12 +2,7 @@ class SessionController < ApplicationController
   def create
     @session = Session.new session_params
 
-<<<<<<< Updated upstream
     if session[:current_user] = @session.auth
-=======
-    if current_user = @session.auth
-      session[:current_user] = { email: current_user.email, name: current_user.name }
->>>>>>> Stashed changes
       redirect_to root_path
     else
       redirect_to users_signin_path
