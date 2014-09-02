9b01d888168ce2c7213dad5a42b26ef81dbe4c0c
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-09-02 18:44:56 -0300

    Show some status information in home page; improvements on tasks chart

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @tasks = current_user.tasks.where('completed_at >= ?', 30.days.ago).group('date(completed_at)').count.to_a
-    @tasks.insert(0, ['Day', 'Tasks completed'])
-    @tasks
+    if current_user
+      @tasks = current_user.tasks.where('completed_at >= ?', 30.days.ago).group('date(completed_at)').count.to_a
+    else
+      @tasks_count = Task.count
+      @users_count = User.count
+    end
   end
 end
