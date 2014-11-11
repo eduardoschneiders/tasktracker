e125e3cc32f9717764b89bb45b8546c75c5581ec
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-11-11 20:55:18 -0200

    Change seeds to create tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# User.delete_all
+User.delete_all
 Task.delete_all
 
-# user = User.create(name: 'Eduardo', email: 'eduardo.m.schneiders@gmail.com', password: 'p@ssw0rd')
-user = User.first
-5.times do |i|
-  Task.create(name: "Task seed completed #{i}", user: user, completed: true, completed_at: Time.now, created_at: 1.day.ago)
-end
+user = User.create(name: 'Eduardo', email: 'eduardo.m.schneiders@gmail.com', password: 'p@ssw0rd')
 
-5.times do |i|
-  Task.create(name: "Task seed completed #{i}", user: user, completed: true, completed_at: 1.day.ago, created_at: 2.day.ago)
-end
+t_before1 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 8.months.ago)
+t_before2 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 8.months.ago)
+t_before3 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 5.months.ago)
+t_before4 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 5.months.ago)
+t_before5 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 4.months.ago)
+t_before6 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 4.months.ago)
+t_before7 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 3.months.ago)
+t_before8 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 3.months.ago)
+t_before9 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 2.months.ago)
+t_before10 = Task.create(name: "Task seed todo before 30 days", user: user, created_at: 2.months.ago)
 
-5.times do |i|
-  Task.create(name: "Task seed #{i}", user: user, completed: false, created_at: 5.day.ago)
-end
+t1 = Task.create(name: "Task seed todo 1", user: user, created_at: 29.days.ago)
+t2 = Task.create(name: "Task seed todo 2", user: user, created_at: 25.days.ago)
+t3 = Task.create(name: "Task seed todo 3", user: user, created_at: 20.days.ago)
+t4 = Task.create(name: "Task seed todo 4", user: user, created_at: 15.days.ago)
 
-5.times do |i|
-  Task.create(name: "Task seed #{i}", user: user, completed: false, created_at: 10.day.ago)
-end
+t1.update_attributes(completed: true, completed_at: 28.days.ago)
+t2.update_attributes(completed: true, completed_at: 24.days.ago)
+t3.update_attributes(completed: true, completed_at: 19.days.ago)
+t4.update_attributes(completed: true, completed_at: 14.days.ago)
+
+t_before1.update_attributes(completed: true, completed_at: 26.days.ago)
+t_before2.update_attributes(completed: true, completed_at: 26.days.ago)
+t_before3.update_attributes(completed: true, completed_at: 25.days.ago)
+t_before4.update_attributes(completed: true, completed_at: 20.days.ago)
+t_before5.update_attributes(completed: true, completed_at: 15.days.ago)
+t_before6.update_attributes(completed: true, completed_at: 7.days.ago)
+t_before7.update_attributes(completed: true, completed_at: 7.days.ago)
+t_before8.update_attributes(completed: true, completed_at: 3.days.ago)
+t_before9.update_attributes(completed: true, completed_at: 3.days.ago)
+t_before10.update_attributes(completed: true, completed_at: 1.day.ago)
