3d30c61db2cbd067694e83836ad3970597760bfc
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-11-11 21:07:39 -0200

    Change logic to calculate graph

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,35 +1,35 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      completed_tasks = 0
-      @tasks = []
-      @completed_tasks = current_user.tasks.where('completed_at >= ?', 30.days.ago).group('date(completed_at)').count.to_a
-      @uncompleted_tasks = current_user.tasks.where('completed_at IS NULL').group('date(created_at)').count.to_a
-      @completed_tasks.each do |day|
-        completed_tasks += day[1]
-        uncompleted_tasks = current_user.tasks.where('completed_at IS NULL and created_at <= ?', Date.parse(day[0])).count
-        @tasks << [day[0], completed_tasks, uncompleted_tasks]
-      end
-        binding.pry
-      30.days.map do |day|
+      tasks                   = { done: {}, todo: {} }
+      total_uncompleted_tasks = current_user.tasks.where('created_at < ?', 1.month.ago).count
+      uncompleted_tasks       = current_user.tasks.where('created_at >= ?', 1.month.ago).group('date(created_at)').count.to_a
+      total_completed_tasks   = 0
+      completed_tasks         = current_user.tasks.where('completed_at >= ?', 1.month.ago).group('date(completed_at)').count.to_a
+
+      uncompleted_tasks.each do |task|
+        day = task[0].split('-').last.to_i
+        tasks[:todo][day] = task[1]
       end
 
-      # [date, completed, unconpleted]
-      @completed_tasks = [
-        ['2014-10-05', 4, 26], 
-        ['2014-10-06', 10, 20], 
-        ['2014-10-10', 11, 19], 
-        ['2014-10-15', 20, 10], 
-        ['2014-10-20', 30, 0],
-        ['2014-10-20', 30, 0],
-        ['2014-10-20', 30, 0],
-        ['2014-10-22', 30, 5],
-        ['2014-10-22', 33, 2],
-        ['2014-10-25', 35, 0],
-        ['2014-10-25', 35, 40],
-        ['2014-10-25', 55, 20],
-        ['2014-10-25', 75, 00],
-      ]
+      completed_tasks.each do |task|
+        day = task[0].split('-').last.to_i
+        tasks[:done][day] = task[1]
+      end
+
+      start             = 1.month.ago.to_date
+      stop              = Date.today.to_date
+      @processed_tasks  = []
+      total_done        = 0
+      total_todo        = current_user.tasks.where('created_at < ?', 1.month.ago).count
+
+      (start..stop).each do |date|
+        day         = date.strftime('%d').to_i
+        total_done  += tasks[:done][day] || 0
+        total_todo  += (tasks[:todo][day] || 0)
+        total_todo -= tasks[:done][day] || 0
+        @processed_tasks << { day: day, done: total_done, todo: total_todo }
+      end
     else
       @tasks_count = Task.count
       @users_count = User.count
