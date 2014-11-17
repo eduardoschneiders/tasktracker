986851698fe6fe1260f147892355dfd8c2d767e7
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-11-17 17:40:14 -0200

    Fix tasks calc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,13 +8,17 @@ class WelcomeController < ApplicationController
       completed_tasks         = current_user.tasks.where('completed_at >= ?', 1.month.ago).group('date(completed_at)').count.to_a
 
       uncompleted_tasks.each do |task|
-        day = task[0].split('-').last.to_i
-        tasks[:todo][day] = task[1]
+        day = task[0].split('-').last
+        month = task[0].split('-')[1]
+        key = "#{day}/#{month}"
+        tasks[:todo][key] = task[1]
       end
 
       completed_tasks.each do |task|
-        day = task[0].split('-').last.to_i
-        tasks[:done][day] = task[1]
+        day = task[0].split('-').last
+        month = task[0].split('-')[1]
+        key = "#{day}/#{month}"
+        tasks[:done][key] = task[1]
       end
 
       start             = 1.month.ago.to_date
@@ -24,10 +28,13 @@ class WelcomeController < ApplicationController
       total_todo        = current_user.tasks.where('created_at < ?', 1.month.ago).count
 
       (start..stop).each do |date|
-        day         = date.strftime('%d').to_i
-        total_done  += tasks[:done][day] || 0
-        total_todo  += (tasks[:todo][day] || 0)
-        total_todo -= tasks[:done][day] || 0
+        day         = date.strftime('%d')
+        month       = date.strftime('%m')
+        key         = "#{day}/#{month}"
+        total_done  += tasks[:done][key] || 0
+        total_todo  += tasks[:todo][key] || 0
+        total_todo  -= tasks[:done][key] || 0
+
         @processed_tasks << { day: day, done: total_done, todo: total_todo }
       end
     else
