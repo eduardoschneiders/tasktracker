a85e37fc42a93d0f26ac01b577e93d9e48be494f
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-11-26 15:17:43 -0200

    Improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def active_menu(path)
-   'active' if request.fullpath == path
+    if path.is_a? Array
+      'active' if path.include? request.fullpath
+    else
+      'active' if request.fullpath == path
+    end
   end
 end
