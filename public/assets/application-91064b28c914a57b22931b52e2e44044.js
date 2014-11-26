36c92488c46243735e9486cef1e4e4b62c154353
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-11-26 17:08:17 -0200

    Vendor

--- public/assets/application-91064b28c914a57b22931b52e2e44044.js
+++ public/assets/application-91064b28c914a57b22931b52e2e44044.js
@@ -13666,34 +13666,6 @@ $(document).ready(function (){
     $('#notice').fadeIn('fast').delay(3000).fadeOut('fast');
   }
 })(taskTracker);
-$(document).ready(function(){
-  (function(objectivesTracker){
-    $('table#tasks-list tr td a#complete').on("ajax:success", function(e, data, status, xhr){
-      $(this).parent().parent().addClass('completed');
-      $(this).hide();
-      $(this).parent().find('#uncomplete').show();
-      update_flash(data.message);
-    });
-
-    $('table#tasks-list tr td a#uncomplete').on("ajax:success", function(e, data, status, xhr){
-      $(this).parent().parent().removeClass('completed');
-      $(this).hide();
-      $(this).parent().find('#complete').show();
-      update_flash(data.message);
-    });
-
-
-    $('table#tasks-list tr td a#remove').on("ajax:success", function(e, data, status, xhr){
-      $(this).parent().parent().remove();
-      update_flash(data.message);
-    });
-
-    function update_flash(message){
-      $('#notice').html(message);
-      $('#notice').fadeIn('fast').delay(3000).fadeOut('fast');
-    }
-  })(objectivesTracker);
-});
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
