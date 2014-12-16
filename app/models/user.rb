13a7b50c70b14df9606789cc1eebb25ae88c7bb1
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2014-12-15 22:35:23 -0200

    Improve user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tasks
+  has_many :groups
 
   validates :email, uniqueness: true
   validates_presence_of :name, :email, :password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 describe User do
+  let(:password)  { 'p@ssw0rd' }
+  let(:user)      { User.create(name: 'Eduardo', email: 'email@gmail.com', password: password) }
+  let(:group)     { user.groups.create(name: 'Group A') }
+
   it 'encrypts password'  do
-    user = User.create(name: 'Eduardo', email: 'email@gmail.com', password: 'p@ssw0rd')
     expect(user.password).not_to eql 'p@ssw0rd'
+  end
+
+  it 'has many groups' do
+    user.groups.create(name: 'Group A')
+    user.groups.create(name: 'Group B')
+
+    expect(user.groups.count).to eql 2
+  end
+
+  it 'has many tasks' do
+    user.tasks.create(name: 'Task one', group: group)
+    user.tasks.create(name: 'Task two', group: group)
+
+    expect(user.tasks.count).to eql 2
   end
 end
