5f7a3462f2d8d09848ccf55fd5d534abe8290c6c
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2015-03-31 09:01:53 -0300

    Refactory on managing tasks in real time

--- app/assets/javascripts/controllers/tasks.js
+++ app/assets/javascripts/controllers/tasks.js
@@ -24,7 +24,7 @@
   }
 
   proto._completeTask = function(){
-    $('table.tasks-list').on('ajax:success', 'tr td.actions .complete', function(e, data, status, xhr){
+    $('.row').on('ajax:success', 'table.tasks-list tr td.actions .complete', function(e, data, status, xhr){
       taskTracker.update_flash(data.message);
       self = e.target;
       uncomplete_path = $(self).attr('data-url-uncomplete');
@@ -37,7 +37,7 @@
   }
 
   proto._uncompleteTask = function(){
-    $('table.tasks-list').on('ajax:success', 'tr td.actions .uncomplete', function(e, data, status, xhr){
+    $('.row').on('ajax:success', 'table.tasks-list tr td.actions .uncomplete', function(e, data, status, xhr){
       taskTracker.update_flash(data.message);
       self = e.target;
       complete_path = $(self).attr('data-url-complete');
@@ -49,7 +49,7 @@
   }
 
   proto._removeTask = function(){
-    $('table.tasks-list').on('ajax:success', 'tr td.actions .remove', function(e, data, status, xhr){
+    $('.row').on('ajax:success', 'table.tasks-list tr td.actions .remove', function(e, data, status, xhr){
       self = e.target
       $(self).parent().parent().remove();
       taskTracker.update_flash(data.message);
@@ -57,20 +57,20 @@
   }
 
   proto._editTask = function(){
-    $('table.tasks-list').on('click', 'tr td.name', function(){
+    $('.row').on('click', 'table.tasks-list tr td.name', function(){
       $(this).find('.text-holder').hide();
       $(this).find('.form-holder').show();
       $(this).find('.form-holder').find('input').focus();
     });
 
-    $('table.tasks-list').on('blur', 'tr td.name .form-holder input', function(){
+    $('.row').on('blur', 'table.tasks-list tr td.name .form-holder input', function(){
       var container = $(this).parents('td.name');
 
       container.find('.text-holder').show();
       container.find('.form-holder').hide();
     });
 
-    $('table.tasks-list').on('ajax:success', 'tr td.name', function(e, data, status, xhr){
+    $('.row').on('ajax:success', 'table.tasks-list tr td.name', function(e, data, status, xhr){
       container = $(e.target).parents('td.name');
       var text = container.find('.form-holder input:text').val();
 
@@ -82,8 +82,9 @@
   }
 
   proto._newTask = function(){
-    this.new_name_container.on("ajax:success", function(e, data, status, xhr){
+    $('.row').on("ajax:success", '.new_task table.tasks-list tr td.name', function(e, data, status, xhr){
       var todo_tasks_container = $(this).parents('.new_task').prev().prev().find('table');
+
       $.get('tasks/' + data.task.id + '/html', function(data){
         todo_tasks_container.append(data);
       });
@@ -109,19 +110,19 @@
   }
 
   proto._editGroup = function(){
-    $('.task-group').on('click', '.group-name', function(){
+    $('.row').on('click', '.task-group .group-name', function(){
       $(this).find('.text-holder').hide();
       $(this).find('.form-holder').show();
       $(this).find('.form-holder').find('input').focus();
     });
 
-    $('.task-group').not('.new').on('blur', '.group-name .form-holder input', function(){
+    $('.row').on('blur', ' .task-group:not(.new) .group-name .form-holder input', function(){
       var container = $(this).parents('.group-name');
       container.find('.text-holder').show();
       container.find('.form-holder').hide();
     });
 
-    $('.task-group').not('.new').on('ajax:success', '.group-name', function(e, data, status, xhr){
+    $('.row').on('ajax:success', '.task-group:not(.new) .group-name', function(e, data, status, xhr){
       container = $(e.target).parents('.group-name');
       var text = container.find('.form-holder input:text').val();
 
